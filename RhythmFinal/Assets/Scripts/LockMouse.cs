156f941bb28b91ae9f37e8ae21e6ae215338618a
Jman87 <juanjomartinezv2707@gmail.com>
2026-03-29 00:18:11 -0400

    scene setup

---
+++ RhythmFinal/Assets/Scripts/LockMouse.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LockMouse : MonoBehaviour
+{
+    
+    public bool cursorLocked = true;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (cursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked; // locks cursor
+            Cursor.visible = false; // additionally makes it invisible on game window
+        }
+        else if (!cursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true; // make cursor visible again
+        }
+        
+    }
+
+    
+}
---
+++ RhythmFinal/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 5f;
+    public Vector2 movementInput;
+    private Rigidbody rb;
+
+    public Transform cameraTransform; // Camera
+    
+
+    private void OnMove(InputValue inputValue) // function to make the guy move
+    {
+        movementInput = inputValue.Get<Vector2>();
+
+        Debug.Log("Making sure this works.");
+    }
+
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // assign rigidbody.
+    }
+
+    void FixedUpdate ()
+    {
+        // to make movement based on camera
+        // grabs camera movements
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+
+        // Build movement direction relative to camera
+        Vector3 movement = forward * movementInput.y + right * movementInput.x;
+
+        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
