05bee98da912fc16019e2de4b0f49dac9a0e1883
Lays-Dev <penn2ink@gmail.com>
2026-04-28 01:29:43 -0400

    Credits Menu Set up!

--- RhythmFinal/Assets/Scripts/Menus/MainMenu.cs
+++ RhythmFinal/Assets/Scripts/Menus/MainMenu.cs
@@ -25,5 +25,15 @@ public class MainMenu : MonoBehaviour
         // Async loads the game per frame and doesn't freeze the UI while loading
         SceneManager.LoadSceneAsync(1);
     }
+
+        public void LoadMainMenu()
+    {
+        Debug.Log("Loading the main menu");
+        Time.timeScale = 1f; // Ensure the game is unpaused before quitting
+        AudioListener.pause = false; // Unpause audio before quitting
+        Cursor.visible = true; // Show the cursor before quitting
+        Cursor.lockState = CursorLockMode.None; // Unlock the cursor before quitting
+        SceneManager.LoadScene(0); // Load the main menu scene 
+    }
     
 }
