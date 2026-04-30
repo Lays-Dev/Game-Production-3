0dc36b028023596b314c41b279002da55ece2403
Tralt0r <xavierrosariotorres@gmail.com>
2026-04-29 23:09:11 -0400

    Bossfight Changes

--- RhythmFinal/Assets/rhythm-specific/rhythm-scripts/LaneMulti.cs
+++ RhythmFinal/Assets/rhythm-specific/rhythm-scripts/LaneMulti.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class LaneMulti : MonoBehaviour
 {
@@ -91,6 +92,9 @@ public class LaneMulti : MonoBehaviour
         Cursor.visible = true;
         
         Destroy(RhythmGame);
+
+        SceneManager.LoadScene("TutorialLevel");
+
     }
 
 }
