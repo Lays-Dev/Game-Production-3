47540388ebadad560160b9d788ad9cdedb2f7bb6
Lays-Dev <penn2ink@gmail.com>
2026-05-01 18:27:01 -0400

    Main Menu Controller Compatibility WORKING
    XBox tested only so far

--- RhythmFinal/Assets/Scripts/Menus/MainMenu.cs
+++ RhythmFinal/Assets/Scripts/Menus/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
 
     void Start()
     {
-        EventSystem.current.SetSelectedGameObject(firstButton);
+       // EventSystem.current.SetSelectedGameObject(firstButton);
     }
 
     // PlayGame is for the button to start the game
